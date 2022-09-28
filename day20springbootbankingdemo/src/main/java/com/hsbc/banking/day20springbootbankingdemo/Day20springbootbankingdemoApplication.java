836257bc01d67cc2b049b8ab50548df638cabf31
eswaribala <parameswaribala@gmail.com>
2022-09-28 15:28:18 +0530

    created spring boot project

---
+++ day20springbootbankingdemo/src/main/java/com/hsbc/banking/day20springbootbankingdemo/Day20springbootbankingdemoApplication.java
@@ -0,0 +1,13 @@
+package com.hsbc.banking.day20springbootbankingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day20springbootbankingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day20springbootbankingdemoApplication.class, args);
+	}
+
+}
